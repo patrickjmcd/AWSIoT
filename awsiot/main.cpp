d196fc7cc3caf1d3d12ea1585dda2e48c9fdde30
Patrick McDonagh <patrickjmcd@gmail.com>
2018-02-23 10:12:04 -0600

    Initial Commit

---
+++ awsiot/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  awsiot
+//
+//  Created by Patrick McDonagh on 2/23/18.
+//  Copyright © 2018 Henry Pump. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
